a2730565856873f814301416fe0c8ccb5ef86e71
Yonatan Feldman <yon@milliped.com>
2002-08-09 18:39:32 +0000

    copyright OpenForce -> MIT

--- sql/postgresql/forums-portlet-drop.sql
+++ sql/postgresql/forums-portlet-drop.sql
@@ -1,5 +1,5 @@
 --
---  Copyright (C) 2001, 2002 OpenForce, Inc.
+--  Copyright (C) 2001, 2002 MIT
 --
 --  This file is part of dotLRN.
 --
@@ -20,7 +20,6 @@
 
 -- Drops forums datasources for portal portlets
 
--- Copyright (C) 2001 Openforce, Inc. 
 -- @author Arjun Sanyal (arjun@openforce.net)
 -- @creation-date 2001-30-09
 
