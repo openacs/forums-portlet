577a06265d378634451cf4f4380fb7ec04f20ac7
Peter Marklund <peter_marklund@fastmail.fm>
2002-10-23 12:31:39 +0000

    making the portlet pretty name use the message catalog

--- tcl/forums-portlet-procs.tcl
+++ tcl/forums-portlet-procs.tcl
@@ -33,7 +33,7 @@ namespace eval forums_portlet {
 
     ad_proc -public get_pretty_name {
     } {
-	return "Bulletin Boards"
+	return "#forums-portlet.pretty_name#"
     }
 
     ad_proc -private my_package_key {
