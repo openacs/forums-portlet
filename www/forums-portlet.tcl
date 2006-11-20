9a053491a985b93ef306399ffdbc59f46887e6b8
Nima Mazloumi <nima.mazloumi@gmx.de>
2006-11-20 20:00:26 +0000

    Mannheim update, fix and merge bugbash

--- www/forums-portlet.tcl
+++ www/forums-portlet.tcl
@@ -20,6 +20,10 @@ set shaded_p $config(shaded_p)
 set list_of_package_ids $config(package_id)
 set one_instance_p [ad_decode [llength $list_of_package_ids] 1 1 0]
 
+# get the parameter from forums-portlet to set the new-graphic correspondingly
+set package_id [apm_package_id_from_key [forums_portlet::my_package_key]]
+set default_new_period [parameter::get -package_id $package_id -parameter DefaultPeriodNewGraphic -default 2]
+
 set query select_forums
 
 if { [acs_privacy::privacy_control_enabled_p] } {
@@ -29,5 +33,26 @@ if { [acs_privacy::privacy_control_enabled_p] } {
 }
 
 set user_id [ad_conn user_id]
+
+template::list::create -name forums -multirow forums -key forum_id -pass_properties {
+} -elements {
+    item {
+        label ""
+        display_template {
+        <b>@forums.parent_name@</b></br>
+          <group column="package_id">
+              <if @forums.new_p@ eq t>
+	        &raquo; <a href="@forums.url@forum-view?forum_id=@forums.forum_id@">@forums.name@</a>
+                <span class="new_flag">
+                  #forums-portlet.resources_acs-subsite_new_gif#</br>
+                </span>
+              </if>
+              <else>
+	        &raquo; <a href="@forums.url@forum-view?forum_id=@forums.forum_id@">@forums.name@</a></br>
+	      </else>
+          </group>
+        }
+    }
+}
 
 db_multirow forums $query {}
